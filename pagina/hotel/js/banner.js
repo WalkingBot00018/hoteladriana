398f730363bbe1fd917fd172949a25616d777bbb
ruben <rlozanotique@gmail.com>
2023-09-03 19:35:11 -0500

    actualizacion3

--- pagina/hotel/js/banner.js
+++ pagina/hotel/js/banner.js
@@ -1,52 +1,42 @@
-var contenedor=('#caja');
-
-var siguiente = ('btn-adelante');
-var anterior = ('btn-atras');
-
+var contenedor = $('#caja');
+var siguiente = $('#btn-adelante');
+var anterior = $('#btn-atras');
 
 $('#caja .section_caja:last').insertBefore('#caja .section_caja:first');
+contenedor.css('margin-left', '-100%');
 
-contenedor.css('margin-left','-',+ 100 + '%');
-
-
-function derecha(){
+function derecha() {
     contenedor.animate({
-        marginleft:'-'+200+'%'
-    },700,function(){
-        $('#caja .section_caja:first').insertAfter('#caja .section_caja:last')
-        contenedor.css('marginleft', '-'+100+'%');
-
+        marginLeft: '-200%'
+    }, 700, function () {
+        $('#caja .section_caja:first').insertAfter('#caja .section_caja:last');
+        contenedor.css('margin-left', '-100%');
     });
+}
 
-};
-
-function isquierda(){
+function izquierda() {
     contenedor.animate({
-        marginleft:0
-    }, 700, function(){
-        $('#caja .section_caja:last').insertBefore('#caja .section_caja:first')
-        contenedor.css('margin-left','-'+100+'%');
+        marginLeft: '0'
+    }, 700, function () {
+        $('#caja .section_caja:last').insertBefore('#caja .section_caja:first');
+        contenedor.css('margin-left', '-100%');
     });
-};
+}
 
-function automatico(){
-    interval = setInterval(function(){
+function automatico() {
+    interval = setInterval(function () {
         derecha();
-    },5000);
+    }, 5000);
+}
 
-};
-
-
-
-siguiente.on('click', function(){
+siguiente.on('click', function () {
     derecha();
     clearInterval(interval);
     automatico();
 });
 
-
-anterior.on('click', function(){
-    isquierda();
+anterior.on('click', function () {
+    izquierda();
     clearInterval(interval);
     automatico();
 });
